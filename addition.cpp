41a50cb74e44d97b659a1d31124494d96d695ec6
devamcodes <devamagrawal.codes@gmail.com>
2020-09-04 20:03:10 +0530

    adds three numbers.

---
+++ addition.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int a;
+
+int main()
+{
+    int x,y;
+    int z;
+    x = y + 2;
+    y = z + 3;
+    cout << "enter a number:-";
+    cin>> z;
+    a = x+y+z;
+    cout << "your number is"<<" ="<<" "<<a;
+
+
+    return 0;
+}
